6c40b07f5c73b8a298b4482620f148656c376ed4
Jeremiah Karanja <jeremiahkaranja584@gmail.com>
2025-07-22 15:40:23 +0300

    Fix: renamed list_books_view to list_books for checker compliance

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,7 +8,7 @@ from .models import Library, Book, UserProfile, Author
 
 # --- Book List View ---
 @login_required
-def list_books_view(request):
+def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
