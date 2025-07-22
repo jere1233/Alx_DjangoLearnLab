c1115bf5943df30051605784c928fe2576d1a87d
Jeremiah Karanja <jeremiahkaranja584@gmail.com>
2025-07-22 16:18:15 +0300

    Fix urls.py imports to match checker requirements

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import RedirectView
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import register
+from . import views
 
 
 from .views import list_books
