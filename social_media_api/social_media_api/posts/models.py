7593e4abab640c90dd0a3e23213cb305aa19b2cd
Jeremiah Karanja <jeremiahkaranja584@gmail.com>
2025-08-30 09:55:06 +0300

    new changes

--- social_media_api/social_media_api/posts/models.py
+++ social_media_api/social_media_api/posts/models.py
@@ -1,33 +1,10 @@
 from django.db import models
-from accounts.models import CustomUser
+from django.contrib.auth.models import User
 
 class Post(models.Model):
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
-    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'Comment by {self.author.username}'
-
-class Like(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ('post', 'user')
-
-    def __str__(self):
-        return f'{self.user.username} likes {self.post.title}'
+        return f"{self.author.username}: {self.content[:30]}"
